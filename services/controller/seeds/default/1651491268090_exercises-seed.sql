d9d27303952773f4d943598cb7ad1c92e968353e
vodoanhoanglong <vodoanhoanglong10a9@gmail.com>
2022-05-02 19:00:04 +0700

    Add service

---
+++ services/controller/seeds/default/1651491268090_exercises-seed.sql
@@ -0,0 +1,12 @@
+SET check_function_bodies = false;
+INSERT INTO public.exercises (id, name, des, level, image, output, input, topic, status, "createdAt", "createdBy", "updatedAt", "updatedBy") VALUES ('86d8b40f-49ab-489d-94d2-fed37f94b66f', 'Làm quen với C++', 'In ra màn hình câu "Hello World"', 1, NULL, '[{"point": 10, "content": "hello world"}]', '[]', 'C++', '"active"', '2022-04-25 14:12:57.872723+00', NULL, '2022-04-25 14:12:57.872723+00', NULL);
+INSERT INTO public.exercises (id, name, des, level, image, output, input, topic, status, "createdAt", "createdBy", "updatedAt", "updatedBy") VALUES ('8639eb5e-4286-4a03-80a8-3a96ae6d0473', 'Phép cộng', 'Cộng 2 số A và B', 1, NULL, '[{"point": 5, "content": "10"}, {"point": 5, "content": "15"}]', '[{"content": "5 5"}, {"content": "8 7"}]', 'C++', '"active"', '2022-05-02 10:33:48.845567+00', NULL, '2022-05-02 10:33:48.845567+00', NULL);
+INSERT INTO public.exercises (id, name, des, level, image, output, input, topic, status, "createdAt", "createdBy", "updatedAt", "updatedBy") VALUES ('c260acdf-5436-47e0-8dcc-c2f1767ff82d', 'Odd Divisor Count', 'Bạn được cho 2 số nguyên A,B. Bạn hãy đếm số lượng số trong [A,B] sao cho số đó có số lượng lẻ ước nguyên dương.  
+Ví dụ:  
+   Với A = 4, B = 4 thì ta có đáp án là 1 số thỏa điều kiện. 
+   Giải thích: 4 có 3 ước là 1,2,4 nên thỏa điều kiện để bài. 
+[Đầu vào/ Đầu ra]:  
+    [Giới hạn thời gian]: 0.5s với C++, 3s với Java & C#, 4s với Python,Go,Js. 
+    [Đầu vào]: Số tự nhiên a (1 ≤ a ≤ 1000). 
+    [Đầu vào]: Số tự nhiên b (a ≤ b ≤ 1000). 
+    [Đầu ra]: Số lượng số trong [a,b] sao cho số đó có số lượng lẻ ước nguyên dương.', 2, NULL, '[{"point": 2, "content": "1"}, {"point": 2, "content": "3"}, {"point": 2, "content": "1"}, {"point": 2, "content": "3"}, {"point": 2, "content": "5"}]', '[{"content": "4 4"}, {"content": "1 10"}, {"content": "5 15"}, {"content": "50 120"}, {"content": "45 130"}]', 'C++', '"active"', '2022-04-25 14:33:30.481496+00', NULL, '2022-04-25 14:33:30.481496+00', NULL);
